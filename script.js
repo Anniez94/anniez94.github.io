85ab14b5a6d634171c1e291ac42138f4ee82ed45
Anniez94 <anniemok94@gmail.com>
2019-03-28 02:41:29 +0100

    root html updated

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 var hamburger = document.querySelector(".hamburger");
-var btnShow = document.querySelector("div")[2];
-var menu = document.getElementsByTagName("div")[3];
-var showHamburger= document.getElementsByTagName("div")[2];
+var menu = document.querySelector(".menu");
 
 hamburger.addEventListener("click", function(){
+    menu.classList.toggle("menu-show");
     menu.classList.toggle("menu");
-    menu.classList.toggle("menu-showcase");
 });
